c065ed8a17ab59f87f62222459003224c1f53d3a
Laszlo Kereki <xaddew@gmail.com>
2020-02-20 13:10:35 +0100

    Feat(LaciAuth): Basic authentication is added, Bootstrap template also.

--- src/main/java/com/springles/tickets/security/WebSecurityConfig.java
+++ src/main/java/com/springles/tickets/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.springles.tickets.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -27,6 +28,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .logout()
         .permitAll();
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web
+        .ignoring()
+        .antMatchers("/resources/**", "/static/**","/webjars/**");
   }
 
   @Bean
